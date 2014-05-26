3604fff74206968b8907f0900115765821f40591
Jeff Fate <fatejd@gmail.com>
2014-05-25 19:40:32 -0700

    Add new tests.

--- src/test/java/com/jeffthefate/TriviaTest.java
+++ src/test/java/com/jeffthefate/TriviaTest.java
@@ -3,6 +3,7 @@ package com.jeffthefate;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map.Entry;
 
 import org.apache.commons.collections.MapIterator;
@@ -41,6 +42,13 @@ public class TriviaTest extends TestCase {
 	 */
 	public static Test suite() {
 		return new TestSuite( TriviaTest.class );
+	}
+	
+	public void testTriviaScreenshot() {
+		setupAnswerMap();
+		Trivia trivia = new Trivia("D:\\setlist.jpg", "D:\\roboto.ttf",
+				"Top Scores", 30, 20, 10, 80, setupTweet(), 0, 0, nameMap, 
+				acronymMap, replaceList, tipList, true, "", 0);
 	}
 
 	/**
@@ -116,87 +124,151 @@ public class TriviaTest extends TestCase {
 	}
 	
 	private static ArrayList<ArrayList<String>> nameMap = new ArrayList<ArrayList<String>>(0);
-    private static ArrayList<ArrayList<String>> acronymMap = new ArrayList<ArrayList<String>>(0);
+	private static HashMap<String, String> acronymMap = new HashMap<String, String>();
+	private static ArrayList<String> replaceList = new ArrayList<String>(0);
+	private static ArrayList<String> tipList = new ArrayList<String>(0);
 	
     private static void setupAnswerMap() {
-    	ArrayList<String> tempList = new ArrayList<String>(0);
-    	tempList.add("dave");
-    	tempList.add("dave matthews");
-    	nameMap.add(tempList);
-    	tempList = new ArrayList<String>(0);
-    	tempList.add("boyd");
-    	tempList.add("boyd tinsley");
-    	nameMap.add(tempList);
-    	tempList = new ArrayList<String>(0);
-    	tempList.add("stefan");
-    	tempList.add("stefan lessard");
-    	nameMap.add(tempList);
-    	tempList = new ArrayList<String>(0);
-    	tempList.add("carter");
-    	tempList.add("carter beauford");
-    	nameMap.add(tempList);
-    	tempList = new ArrayList<String>(0);
-    	tempList.add("leroi");
-    	tempList.add("leroi moore");
-    	tempList.add("roi");
-    	nameMap.add(tempList);
-    	tempList = new ArrayList<String>(0);
-    	tempList.add("butch");
-    	tempList.add("butch taylor");
-    	nameMap.add(tempList);
-    	tempList = new ArrayList<String>(0);
-    	tempList.add("tim");
-    	tempList.add("tim reynolds");
-    	nameMap.add(tempList);
-    	tempList = new ArrayList<String>(0);
-    	tempList.add("jeff");
-    	tempList.add("jeff coffin");
-    	tempList.add("coffin");
-    	nameMap.add(tempList);
-    	tempList = new ArrayList<String>(0);
-    	tempList.add("rashawn");
-    	tempList.add("rashawn ross");
-    	nameMap.add(tempList);
-    	tempList = new ArrayList<String>(0);
-    	tempList.add("lillywhite");
-    	tempList.add("steve lillywhite");
-    	nameMap.add(tempList);
-    	tempList = new ArrayList<String>(0);
-    	tempList.add("sax");
-    	tempList.add("saxophone");
-    	nameMap.add(tempList);
-    	tempList = new ArrayList<String>(0);
-    	tempList.add("alpine");
-    	tempList.add("alpine valley");
-    	nameMap.add(tempList);
-    	/*
-    	nameMap.put("dave", "dave matthews");
-    	nameMap.put("boyd", "boyd tinsley");
-    	nameMap.put("stefan", "stefan lessard");
-    	nameMap.put("carter", "carter beauford");
-    	nameMap.put("leroi moore", "leroi");
-    	nameMap.put("roi", "leroi moore");
-    	nameMap.put("leroi", "roi");
-    	nameMap.put("butch", "butch taylor");
-    	nameMap.put("tim", "tim reynolds");
-    	nameMap.put("jeff", "jeff coffin");
-    	nameMap.put("jeff coffin", "coffin");
-    	nameMap.put("rashawn", "rashawn ross");
-    	nameMap.put("lillywhite", "steve lillywhite");
-    	acronymMap.put("btcs", "before these crowded streets");
-    	acronymMap.put("uttad", "under the table and dreaming");
-    	nameMap.put("sax", "saxophone");
-    	acronymMap.put("watchtower", "all along the watchtower");
-    	acronymMap.put("hunger", "hunger for the great light");
-    	acronymMap.put("crash", "crash into me");
-    	acronymMap.put("nancies", "dancing nancies");
-    	acronymMap.put("big whiskey", "big whiskey and the groogrux king");
-    	acronymMap.put("msg", "madison square garden");
-    	nameMap.put("alpine", "alpine valley");
-    	acronymMap.put("wpb", "west palm beach");
-    	*/
-    	// TODO Add pronouns
-    }
+		ArrayList<String> tempList = new ArrayList<String>(0);
+		tempList.add("dave");
+		tempList.add("dave matthews");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("boyd");
+		tempList.add("boyd tinsley");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("stefan");
+		tempList.add("stefan lessard");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("carter");
+		tempList.add("carter beauford");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("leroi");
+		tempList.add("leroi moore");
+		tempList.add("roi");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("butch");
+		tempList.add("butch taylor");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("tim");
+		tempList.add("tim reynolds");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("jeff");
+		tempList.add("jeff coffin");
+		tempList.add("coffin");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("rashawn");
+		tempList.add("rashawn ross");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("lillywhite");
+		tempList.add("steve lillywhite");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("lawlor");
+		tempList.add("joe lawlor");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("fenton");
+		tempList.add("fenton williams");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("peter");
+		tempList.add("peter griesar");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("sax");
+		tempList.add("saxophone");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("alpine");
+		tempList.add("alpine valley");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("big whiskey and the groogrux king");
+		tempList.add("big whiskey");
+		tempList.add("big whiskey & the groogrux king");
+		tempList.add("bwggk");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("stay");
+		tempList.add("stay (wasting time)");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("you and me");
+		tempList.add("you & me");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("rhyme and reason");
+		tempList.add("rhyme & reason");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("funny the way it is");
+		tempList.add("ftwii");
+		tempList.add("funny");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("sweet up & down");
+		tempList.add("sweet up and down");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("billies");
+		tempList.add("tripping billies");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("bela fleck and the flecktones");
+		tempList.add("bela fleck & the flecktones");
+		tempList.add("the flecktones");
+		nameMap.add(tempList);
+		tempList = new ArrayList<String>(0);
+		tempList.add("any noise");
+		tempList.add("any noise/anti-noise");
+		tempList.add("any noise anti-noise");
+		tempList.add("any noise anti noise");
+		tempList.add("any noise antinoise");
+		tempList.add("anynoise antinoise");
+		nameMap.add(tempList);
+		acronymMap.put("btcs", "before these crowded streets");
+		acronymMap.put("uttad", "under the table and dreaming");
+		acronymMap.put("watchtower", "all along the watchtower");
+		acronymMap.put("hunger", "hunger for the great light");
+		acronymMap.put("crash", "crash into me");
+		acronymMap.put("nancies", "dancing nancies");
+		acronymMap.put("msg", "madison square garden");
+		acronymMap.put("wpb", "west palm beach");
+		acronymMap.put("ddtw", "dont drink the water");
+		replaceList.add("the ");
+		replaceList.add("his ");
+		replaceList.add("her ");
+		// tipList.add("1st place gets full points, 2nd place 75% full points, 3rd place 50% full points");
+		// tipList.add("There are three rounds: first uses regular point values, second adds 500 points extra, bonus round adds 1000 extra");
+		/*
+		 * nameMap.put("dave", "dave matthews"); nameMap.put("boyd",
+		 * "boyd tinsley"); nameMap.put("stefan", "stefan lessard");
+		 * nameMap.put("carter", "carter beauford"); nameMap.put("leroi moore",
+		 * "leroi"); nameMap.put("roi", "leroi moore"); nameMap.put("leroi",
+		 * "roi"); nameMap.put("butch", "butch taylor"); nameMap.put("tim",
+		 * "tim reynolds"); nameMap.put("jeff", "jeff coffin");
+		 * nameMap.put("jeff coffin", "coffin"); nameMap.put("rashawn",
+		 * "rashawn ross"); nameMap.put("lillywhite", "steve lillywhite");
+		 * acronymMap.put("btcs", "before these crowded streets");
+		 * acronymMap.put("uttad", "under the table and dreaming");
+		 * nameMap.put("sax", "saxophone"); acronymMap.put("watchtower",
+		 * "all along the watchtower"); acronymMap.put("hunger",
+		 * "hunger for the great light"); acronymMap.put("crash",
+		 * "crash into me"); acronymMap.put("nancies", "dancing nancies");
+		 * acronymMap.put("big whiskey", "big whiskey and the groogrux king");
+		 * acronymMap.put("msg", "madison square garden"); nameMap.put("alpine",
+		 * "alpine valley"); acronymMap.put("wpb", "west palm beach");
+		 */
+	}
 	
 	private static Configuration setupTweet() {
     	ConfigurationBuilder cb = new ConfigurationBuilder();
